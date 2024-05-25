bac54f03f4cf1cff0e8adb08249757a108e35b08
desurkumaran777 <desurkumaran777@gmail.com>
2024-05-25 19:18:14 +0530

    Initial task model has been done without migration

--- task_controller/models.py
+++ task_controller/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Task(models.Model):
+    PRIORITY_CHOICES = [
+        ('H', 'High'),
+        ('M', 'Medium'),
+        ('L', 'Low'),
+    ]
+
+    STATUS_CHOICES = [
+        ('P', 'Pending'),
+        ('I', 'In Progress'),
+        ('C', 'Completed'),
+    ]
+
+    task_id = models.AutoField(primary_key=True)
+    task_title = models.CharField(max_length=255)
+    task_desc = models.TextField()
+    task_priority = models.CharField(max_length=1, choices=PRIORITY_CHOICES)
+    task_status = models.CharField(max_length=1, choices=STATUS_CHOICES)
+
+    def __str__(self):
+        return self.task_title
+
+    class Meta:
+        db_table = 'task_tbl'
